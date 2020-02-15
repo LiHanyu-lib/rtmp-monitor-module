bffbefea9faec829a461adfe7bad3f4a8d0c358d
lihanyu <li1289794166@163.com>
2020-02-15 17:38:20 +0800

    feature: 抓取服务器 cpu、mem 监控，以及获取推流信息

--- server.js
+++ server.js
@@ -1,86 +1,19 @@
-var express = require("express");
+// var express = require("express");
 // var session = require('express-session');
 var request = require('request');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var parseString = require('xml2js').parseString;
 var config = require('./config.json');
 // var language = require('./language/'+config.language+'.json');
-var app = express();
+// var app = express();
 // app.use(express.static('public'));
 // app.use(bodyParser.urlencoded({extended: false}));
 // var server = app.listen(config.http_server_port);
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+// var server = require('http').createServer(app);
+var server = require('http').createServer();
+// var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(config.http_server_port);
 var os = require("os");
-// var swig = require('swig');
-// var swig = new swig.Swig();
-// app.engine('html', swig.renderFile);
-// app.set('view engine', 'html');
-// app.set('views', __dirname + '/template/');
-
-
-// function customHeaders( req, res, next ){
-//     app.disable( 'x-powered-by' );
-//     res.setHeader( 'X-Powered-By', "fstv monitoring "+config.version );
-//     next()
-// }
-
-// app.use(customHeaders);
-
-// app.use(session({
-//     secret: config.session_secret_key,
-//     resave: true,
-//     saveUninitialized: true
-// }));
-
-// var auth = function (req, res, next) {
-//     if (req.session && req.session.user === "fstn" && req.session.admin) {
-//         return next();
-//     }
-//     else {
-//         res.redirect("/login");
-//     }
-// };
-
-// app.get('/logout', function (req, res) {
-//     req.session.destroy();
-//     res.redirect("/login");
-// });
-
-// app.post('/login', function (req, res) {
-//     if (!req.body.username || !req.body.password) {
-//         return res.redirect("/login?error=empty");
-//     }
-
-//     if (req.body.username === config.username && req.body.password === config.password) {
-//         req.session.user = "fstn";
-//         req.session.admin = true;
-//         return res.redirect("/");
-//     }
-//     return res.redirect("/login?error=wrong");
-// });
-
-// app.get('/login', function (req, res) {
-//     res.render(config.login_template, {
-//         title: config.site_title,
-//         language: language,
-//         error: req.query.error
-//     });
-// });
-
-// app.get('/',auth,function(req,res){
-//    res.render(config.template,{
-//         title: config.site_title,
-//         language: language,
-//         version:config.version,
-//         stream_server:config.rtmp_server_stream_url,
-//         control_server:config.rtmp_server_control_url
-//    });
-// });
-
-// app.get('*', function(req, res){
-//     res.redirect('/');
-// });
 
 function cpuAverage() {
     var totalIdle = 0, totalTick = 0;
@@ -115,6 +48,7 @@ setInterval(function(){
                 lines[i] = lines[i].split(/\s+/);
             }
             used_memory = lines[1][2];
+            console.log('used_CPU=' + percentageCPU + '  used_memory=' + used_memory);
             io.emit('server', {
                 used_cpu:percentageCPU,
                 used_memory:used_memory
@@ -125,13 +59,15 @@ setInterval(function(){
     request.get(config.rtmp_server_url,{timeout:config.rtmp_server_timeout},function(error, meta, xml){
 
     console.log('------------------------')
-    console.log(xml)
+    // console.log(xml)
 
     if(error) {
         io.emit('error', {"error":true});
     }
 
     parseString(xml,function (err, result) {
+
+        console.log(result)
 
     	if(err != null)
     	{
